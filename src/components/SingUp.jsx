3404635360eaa12a80f1e4399e4fdf30e6bc76fc
MAHABUB <info.freelancermahabub@gmail.com>
2023-04-24 13:32:42 +0600

    newadded

--- src/components/SingUp.jsx
+++ src/components/SingUp.jsx
@@ -55,7 +55,7 @@ const SingUp = () => {
                 htmlFor="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Password da
+                Password daa
               </label>
               <input
                 type={passwordShown ? "text" : "password"}
